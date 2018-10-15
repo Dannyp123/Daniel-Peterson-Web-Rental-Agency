f32fdc7f4003c079bc6df60fa11a6920f4492ca0
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-10-15 11:26:46 -0500

    After making hard data inventory, I started to break them down into templates

--- data.js
+++ data.js
@@ -1,11 +1,24 @@
 const INVENTORY = {
-  cars: {
-    make: "Ford",
-    model: "Mustang",
-    color: "Flat Black",
-    year: 2013,
-    description:
-      "2013 Ford Mustang, great condition. New transmition and new 545 Big Block.",
-    price: 34000
-  }
+  vehicles: [
+    {
+      make: "Ford",
+      model: "Mustang",
+      color: "Red",
+      year: 2013,
+      description:
+        "2013 Ford Mustang, great condition. New transmition and new 545 Big Block.",
+      instock: 34,
+      priceperday: 250
+    },
+    {
+      make: "Chevy",
+      model: "Camero",
+      color: "Grey",
+      year: 2015,
+      description:
+        "2015 Chevy Camero, runs great and is super fast. Tinted windows and new interior",
+      instock: 28,
+      priceperday: 325
+    }
+  ]
 };
--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,29 @@
-function showingInventory() {
-  const show = document.querySelector("#inventoryLink");
-  var firstItem = document.getElementById("first-item");
-  show.addEventListener("click", function() {
-    firstItem.innerHTML = INVENTORY.cars.make;
-  });
+function settingUpInventory() {
+  var inventoryItems = document.querySelector("#template-inventory").innerHTML;
+  var template = Handlebars.compile(inventoryItems);
+  var total = INVENTORY.vehicles.length;
+  var html = "";
+  for (vehicle = 0; vehicle < total; vehicle++) {
+    html += template({
+      make: `${INVENTORY.vehicles[vehicle].make}`,
+      model: `${INVENTORY.vehicles[vehicle].model}`
+    });
+  }
+  document.querySelector("#insert-template-here").innerHTML = html;
 }
-showingInventory();
+
+// function settingUpInventory() {
+//   const make = document.querySelector(".make-info");
+//   const model = document.querySelector(".model-info");
+//   const year = document.querySelector(".year-info");
+//   const inStock = document.querySelector(".stock-info");
+//   const price = document.querySelector(".price-info");
+
+//   make.innerText = INVENTORY.car1.make;
+//   model.innerText = INVENTORY.car1.model;
+//   year.innerText = INVENTORY.car1.color;
+//   inStock.innerText = INVENTORY.car1.instock;
+//   price.innerText = `$${INVENTORY.car1.priceperday}`;
+// }
+
+settingUpInventory();
