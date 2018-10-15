ff3b4e74b1b3bbcb49d529a2091647d3c8412372
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-10-15 13:08:06 -0500

    Made the templates, now just adding cars to the invertory

--- data.js
+++ data.js
@@ -8,7 +8,8 @@ const INVENTORY = {
       description:
         "2013 Ford Mustang, great condition. New transmition and new 545 Big Block.",
       instock: 34,
-      priceperday: 250
+      priceperday: 250,
+      image: "images-using/ford-mustang2.jpeg"
     },
     {
       make: "Chevy",
@@ -18,7 +19,19 @@ const INVENTORY = {
       description:
         "2015 Chevy Camero, runs great and is super fast. Tinted windows and new interior",
       instock: 28,
-      priceperday: 325
+      priceperday: 325,
+      image: "images-using/chevy-camero.jpg"
+    },
+    {
+      make: "Dodge",
+      model: "Challenger Hellcat",
+      color: "Orange",
+      year: 2016,
+      description:
+        "2016 Dodge Challenger The Hellcat, Yes I said the Hellcat. Has a 6.8 L V8 engine, and can get up to 60MPH in an amazing 4.6 seconds.",
+      instock: 21,
+      priceperday: 563,
+      image: "images-using/dodge-challenger.jpeg"
     }
   ]
 };
--- javaScript.js
+++ javaScript.js
@@ -1,29 +1,21 @@
 function settingUpInventory() {
   var inventoryItems = document.querySelector("#template-inventory").innerHTML;
   var template = Handlebars.compile(inventoryItems);
-  var total = INVENTORY.vehicles.length;
-  var html = "";
-  for (vehicle = 0; vehicle < total; vehicle++) {
-    html += template({
-      make: `${INVENTORY.vehicles[vehicle].make}`,
-      model: `${INVENTORY.vehicles[vehicle].model}`
+  for (vehicle of INVENTORY.vehicles) {
+    var html = template({
+      make: `${vehicle.make}`,
+      model: `${vehicle.model}`,
+      year: `${vehicle.year}`,
+      description: vehicle.description,
+      instock: `${vehicle.instock}`,
+      priceperday: `$${vehicle.priceperday}`,
+      color: `${vehicle.color}`,
+      image: vehicle.image
     });
+    document
+      .querySelector("#insert-template-here")
+      .insertAdjacentHTML("beforeend", html);
   }
-  document.querySelector("#insert-template-here").innerHTML = html;
 }
-
-// function settingUpInventory() {
-//   const make = document.querySelector(".make-info");
-//   const model = document.querySelector(".model-info");
-//   const year = document.querySelector(".year-info");
-//   const inStock = document.querySelector(".stock-info");
-//   const price = document.querySelector(".price-info");
-
-//   make.innerText = INVENTORY.car1.make;
-//   model.innerText = INVENTORY.car1.model;
-//   year.innerText = INVENTORY.car1.color;
-//   inStock.innerText = INVENTORY.car1.instock;
-//   price.innerText = `$${INVENTORY.car1.priceperday}`;
-// }
 
 settingUpInventory();
