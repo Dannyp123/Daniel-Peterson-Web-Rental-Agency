b9fe96bdea94caae58b1ada7d3cc478700268951
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-10-15 22:29:29 -0500

    with some help, finally got the stock to change and made the in cart area and started the form to fill when checking out

--- javaScript.js
+++ javaScript.js
@@ -18,43 +18,57 @@ function settingUpInventory() {
   }
 }
 
-function workingNavbar() {
-  const home = document.querySelector("#home");
-  const homeLink = document.querySelector("#home-link");
-  homeLink.addEventListener("click", function() {
-    home.style.display = "block";
+function workingCart() {
+  const cart = document.querySelector("#cart");
+  const cartLink = document.querySelector("#cart-link");
+  cartLink.addEventListener("click", function() {
+    cart.style.display = "block";
   });
 }
 
 function workingForms() {
-  const form = document.querySelector("#form-inputs");
-  const formLink = document.querySelector("#cart-link");
+  const form = document.querySelector("#check-out");
+  const formLink = document.querySelector("#checkout-link");
   formLink.addEventListener("click", function() {
     form.style.display = "block";
   });
 }
 
-function addingToCart() {
-  var rentButton = document.querySelector(".rentingButton");
-  var checkoutArea = document.querySelector("#checkoutInput");
-  for (car in INVENTORY.vehicles) {
-    rentButton.addEventListener("click", function() {
-      checkoutArea.innerHTML = car.make;
+function removeStock(cardIndex) {
+  var cards = document.querySelectorAll(".itemCard");
+  cards[cardIndex].querySelector(".cardStock").innerHTML =
+    cards[cardIndex].querySelector(".cardStock").innerHTML - 1;
+}
+
+function clickingRentButton() {
+  var cards = document.querySelectorAll(".itemCard");
+  cards.forEach(function(button, index) {
+    button.addEventListener("click", function() {
+      if (cards[index].querySelector(".cardStock").innerText > 0) {
+        removeStock(index);
+      }
     });
+  });
+}
+
+function addingToCart() {
+  var inCart = document.querySelector(".shoppingCartNum");
+  inCart.innerText = Number(inCart.innerText) + 1;
+}
+
+function placingTheOrder() {
+  var cartNum = Number(document.querySelector(".shoppingCartNum").innerText);
+  if (cartNum > 0) {
+    document.querySelector("#checkOutButton").disabled = false;
+  } else {
+    document.querySelector("#checkOutButton").disabled = true;
   }
 }
-// function settingRentingButtonUp() {
-//   for (stock of INVENTORY.vehicles) {
-//     var rentButton = document.querySelector(".rentingButton");
-//     var num = stock.instock;
-//     rentButton.addEventListener("click", function() {
-//       num--;
-//     });
-//   }
-// }
 
 settingUpInventory();
-workingNavbar();
+workingCart();
 workingForms();
+clickingRentButton();
+removeStock();
+placingTheOrder();
 addingToCart();
-// settingRentingButtonUp();
