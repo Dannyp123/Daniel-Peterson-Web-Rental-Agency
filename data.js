a21d38c1a63aa35d7e897cbdd3afc5cbf1ab800a
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-10-17 10:59:46 -0500

    Got the total to work, and been adding cars

--- data.js
+++ data.js
@@ -8,30 +8,19 @@ const INVENTORY = {
       description:
         "2018 Ford Mustang, just off the lot. Come rent one so you can be red hot while cruising in this red hot car!",
       instock: 17,
-      priceperday: 425,
+      priceperday: 100,
       image: "images-using/ford-mustang3.jpg"
     },
     {
-      make: "Chevy",
-      model: "Camero",
+      make: "Ford",
+      model: "Fusion Sport",
       color: "Grey",
-      year: 2015,
+      year: 2012,
       description:
-        "2015 Chevy Camero, runs great and is super fast. Tinted windows and new interior",
-      instock: 12,
-      priceperday: 325,
-      image: "images-using/chevy-camero.jpg"
-    },
-    {
-      make: "Dodge",
-      model: "Challenger Hellcat",
-      color: "Orange",
-      year: 2016,
-      description:
-        "2016 Dodge Challenger The Hellcat, Yes I said the Hellcat. Has a 6.8 L V8 engine, and can get up to 60MPH in an amazing 4.6 seconds.",
-      instock: 21,
-      priceperday: 563,
-      image: "images-using/dodge-challenger2.jpg"
+        "2012 Ford Fusion, comes with the 3.5 L V6 engine. Has 259 HP and can hit 60 in 6.9 seconds.",
+      instock: 6,
+      priceperday: 225,
+      image: "images-using/ford-fusionsport.jpg"
     },
     {
       make: "Ford",
@@ -41,8 +30,42 @@ const INVENTORY = {
       description:
         "2017 Ford Raptor, basically brand new only 28,500 miles. Has a 7.5 L V8 and can get up and go.",
       instock: 6,
-      priceperday: 450,
+      priceperday: 170,
       image: "images-using/ford-raptor2.jpeg"
+    },
+    {
+      make: "Chevy",
+      model: "Camaro",
+      color: "Grey",
+      year: 1969,
+      description:
+        "1969 Chevy Camaro, she is an old beaut. Has been treated right and runs great. Has a 545 Big Block with new cams. Come get you a classic!",
+      instock: 6,
+      priceperday: 225,
+      image: "images-using/1969Chevy-camero.jpg"
+    },
+    {
+      make: "Chevy",
+      model: "Camaro",
+      color: "Grey",
+      year: 2015,
+      description:
+        "2015 Chevy Camero, runs great and is super fast. Tinted windows and new interior",
+      instock: 12,
+      priceperday: 125,
+      image: "images-using/chevy-camero.jpg"
+    },
+
+    {
+      make: "Dodge",
+      model: "Challenger Hellcat",
+      color: "Orange",
+      year: 2016,
+      description:
+        "2016 Dodge Challenger The Hellcat, Yes I said the Hellcat. Has a 6.8 L V8 engine, and can get up to 60MPH in an amazing 4.6 seconds.",
+      instock: 21,
+      priceperday: 215,
+      image: "images-using/dodge-challenger2.jpg"
     },
     {
       make: "Jeep",
@@ -63,8 +86,19 @@ const INVENTORY = {
       description:
         "2016 BMW M2, comes with sports addition or standard. Can get it manual or automatic. Comes with stock Kicker sterio system. Although it has a V6 in it, do not let that fool you. She can get up and run!",
       instock: 14,
-      priceperday: 525,
+      priceperday: 225,
       image: "images-using/BMW-m2.jpg"
+    },
+    {
+      make: "Bentley",
+      model: "Continental GT",
+      color: "White",
+      year: 2018,
+      description:
+        "2018 Bently Continental GT Convertible, comes with V8, V8-s, or W-12 engines. Great for a classy ride or a fast take off. It is snow-white with after-market black rims. If you want to be classy and go fast, come get you a Bently!",
+      instock: 4,
+      priceperday: 375,
+      image: "images-using/Bently-contenialGT2.jpg"
     }
   ]
 };
